55c962a4737fb622923d4a0fc5e5774d2dab8c83
Shadoku <Shadoku@gmail.com>
2021-01-18 00:18:33 -0700

    Create download_model.sh

---
+++ download_model.sh
@@ -0,0 +1,18 @@
+  
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "You must enter the model name as a parameter, e.g.: sh download_model.sh 117M"
+    exit 1
+fi
+
+model=$1
+
+mkdir -p trained_model
+
+# TODO: gsutil rsync -r gs://gpt-2/models/ models/
+for filename in checkpoint encoder.json hparams.json model.ckpt.data-00000-of-00001 model.ckpt.index model.ckpt.meta vocab.bpe; do
+  fetch=$model/$filename
+  echo "Fetching $fetch"
+  curl --output models https://storage.googleapis.com/gpt-2/models/$fetch
+done
